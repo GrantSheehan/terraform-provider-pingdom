a7a8266f1d4430f952d68f8c4e3dbc151c23e357
Grant Sheehan <gsheehan@gmail.com>
2018-04-21 09:25:02 -0600

    Fix import path

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/GrantSheehan/terraform-provider-pingdom/pingdom"
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/russellcardullo/terraform-provider-pingdom/pingdom"
 )
 
 func main() {
